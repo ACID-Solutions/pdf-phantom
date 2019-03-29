a6f9dc46c5c4c658b9fe428c74959a225ecedb60
Daniel Lucas <daniel@acid.fr>
2019-03-29 15:42:39 +0100

    Added docblock to functions file

--- src/functions.php
+++ src/functions.php
@@ -1,8 +1,12 @@
 <?php
 
 if (! function_exists('pdf_phantom')) {
+    /**
+     * @return \AcidSolutions\PdfPhantom\PdfGenerator
+     */
     function pdf_phantom()
     {
         return app('pdf_phantom');
     }
 }
+
